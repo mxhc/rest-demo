e9977a053e9e32c00361c8052bd621dccbeafd1c
mxhc <42714610+mxhc@users.noreply.github.com>
2018-12-24 14:58:46 +0100

    added domain and repository base classes. closes #1

---
+++ src/main/java/com/smort/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.smort.repositories;
+
+import com.smort.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
