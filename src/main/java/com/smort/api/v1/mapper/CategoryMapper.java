b6eac454c6b5f5010812bd41f2c4c48837c2d9eb
mxhc <42714610+mxhc@users.noreply.github.com>
2018-12-24 17:14:38 +0100

    add MapStruct support closes #7

---
+++ src/main/java/com/smort/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,15 @@
+package com.smort.api.v1.mapper;
+
+import com.smort.api.v1.model.CategoryDTO;
+import com.smort.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CategoryMapper {
+
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    CategoryDTO categoryToCategoryDTO(Category category);
+
+}
--- src/main/java/com/smort/api/v1/model/CategoryDTO.java
+++ src/main/java/com/smort/api/v1/model/CategoryDTO.java
@@ -1,5 +1,8 @@
 package com.smort.api.v1.model;
 
+import lombok.Data;
+
+@Data
 public class CategoryDTO {
 
     private Long id;
