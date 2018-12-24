3678a5af3118e51c4fd7f630bcaf70dcb3d7637c
mxhc <42714610+mxhc@users.noreply.github.com>
2018-12-24 17:38:20 +0100

    Create tests for object conversion with MapStruct closes #8

---
+++ src/test/java/com/smort/api/v1/mapper/CategoryMapperTest.java
@@ -0,0 +1,32 @@
+package com.smort.api.v1.mapper;
+
+import com.smort.api.v1.model.CategoryDTO;
+import com.smort.domain.Category;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryMapperTest {
+
+    public static final String NAME = "Milojko";
+    public static final long ID = 1L;
+
+    CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+
+    @Test
+    public void categoryToCategoryDTO() {
+
+        // given
+        Category category = new Category();
+        category.setName(NAME);
+        category.setId(ID);
+
+        // when
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+
+        // then
+        assertEquals(Long.valueOf(ID), categoryDTO.getId());
+        assertEquals(NAME, categoryDTO.getName());
+
+    }
+}
