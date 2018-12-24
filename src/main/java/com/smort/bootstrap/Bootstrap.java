fc20eca60bba12f03615eed599650526f25a0154
mxhc <42714610+mxhc@users.noreply.github.com>
2018-12-24 23:13:00 +0100

    Create Bootstrap Data for Customers. closes #12

--- src/main/java/com/smort/bootstrap/Bootstrap.java
+++ src/main/java/com/smort/bootstrap/Bootstrap.java
@@ -1,7 +1,9 @@
 package com.smort.bootstrap;
 
 import com.smort.domain.Category;
+import com.smort.domain.Customer;
 import com.smort.repositories.CategoryRepository;
+import com.smort.repositories.CustomerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,11 @@ import org.springframework.stereotype.Component;
 public class Bootstrap implements CommandLineRunner {
 
     private CategoryRepository categoryRepository;
+    private CustomerRepository customerRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
@@ -41,6 +45,34 @@ public class Bootstrap implements CommandLineRunner {
         categoryRepository.save(nuts);
 
         log.warn("Data Loaded = " + categoryRepository.count());
+
+        Customer c1 = new Customer();
+        c1.setFirstName("Milojko");
+        c1.setLastName("Pantic");
+
+        Customer c2 = new Customer();
+        c2.setFirstName("Milan");
+        c2.setLastName("Petrovic");
+
+        Customer c3 = new Customer();
+        c3.setFirstName("Inga");
+        c3.setLastName("Zica");
+
+        Customer c4 = new Customer();
+        c4.setFirstName("David");
+        c4.setLastName("Zica");
+
+        Customer c5 = new Customer();
+        c5.setFirstName("Janko");
+        c5.setLastName("Petrovic");
+
+        customerRepository.save(c1);
+        customerRepository.save(c2);
+        customerRepository.save(c3);
+        customerRepository.save(c4);
+        customerRepository.save(c5);
+
+        log.warn("Customers loaded = " + customerRepository.count());
 
     }
 }
