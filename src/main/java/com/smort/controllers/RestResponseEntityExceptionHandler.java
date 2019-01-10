5c6f62f197b179751fb27729bb67c3aa72b70710
mxhc <42714610+mxhc@users.noreply.github.com>
2019-01-10 18:26:20 +0100

    xxx

--- src/main/java/com/smort/controllers/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/smort/controllers/RestResponseEntityExceptionHandler.java
@@ -1,11 +1,9 @@
 package com.smort.controllers;
 
 import com.smort.error.ResourceNotFoundException;
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
