7b8d478d76ad88e24c2252793587b174690a119f
mxhc <42714610+mxhc@users.noreply.github.com>
2019-01-11 17:50:03 +0100

    Add support for swagger documenting Validation Constrains #55

--- src/main/java/com/smort/config/SwaggerConfig.java
+++ src/main/java/com/smort/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.smort.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -14,6 +16,7 @@ import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
     @Bean
