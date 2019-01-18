fbc81216b9a6755866ff0773901608cf6296775e
mxhc <42714610+mxhc@users.noreply.github.com>
2019-01-18 03:03:24 +0100

    fixed build fail

--- src/test/java/com/smort/controllers/v1/ProductControllerTest.java
+++ src/test/java/com/smort/controllers/v1/ProductControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ActiveProfiles("no-security")
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = {ProductController.class})
 public class ProductControllerTest {
