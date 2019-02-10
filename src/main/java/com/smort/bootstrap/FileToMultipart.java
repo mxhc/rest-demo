7589338c688b36e06c09e70ade2b821b89299044
mxhc <42714610+mxhc@users.noreply.github.com>
2019-02-10 02:23:22 +0100

    Add Photos to Bootsptrap. closes #89

--- src/main/java/com/smort/bootstrap/FileToMultipart.java
+++ src/main/java/com/smort/bootstrap/FileToMultipart.java
@@ -2,7 +2,9 @@ package com.smort.bootstrap;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.apache.commons.io.FileUtils;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
@@ -16,7 +18,17 @@ public class FileToMultipart {
 //        File diskFile = ResourceUtils.getFile("classpath:" + fileName);
 
 
-        File diskFile = new File("src/main/resources/products/" + fileName);
+//        File diskFile = new File("src/main/resources/products/" + fileName);
+
+        ClassPathResource classPathResource = new ClassPathResource("products/" + fileName);
+
+        InputStream inputStream = classPathResource.getInputStream();
+        File diskFile = File.createTempFile(fileName, ".jpg");
+        try {
+            FileUtils.copyInputStreamToFile(inputStream, diskFile);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
 
         FileItem fileItem = new DiskFileItem("file", Files.probeContentType(diskFile.toPath()), false, diskFile.getName(), (int) diskFile.length(), diskFile.getParentFile());
 
